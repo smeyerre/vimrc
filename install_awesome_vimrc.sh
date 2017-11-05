f60ae1bb43d44b98ebb505e3dfe66a575fda51f6
Sam Meyer-Reed <samwisemr@gmail.com>
2017-11-05 11:50:36 -0500

    update install script

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 set -e
 
-cd ~/.vim_runtime
+cd ~/.vim-runtime
 
-echo 'set runtimepath+=~/.vim_runtime
+echo 'set runtimepath+=~/.vim-runtime
 
-source ~/.vim_runtime/vimrcs/basic.vim
-source ~/.vim_runtime/vimrcs/filetypes.vim
-source ~/.vim_runtime/vimrcs/plugins_config.vim
-source ~/.vim_runtime/vimrcs/extended.vim
+source ~/.vim-runtime/vimrcs/basic.vim
+source ~/.vim-runtime/vimrcs/filetypes.vim
+source ~/.vim-runtime/vimrcs/plugins_config.vim
+source ~/.vim-runtime/vimrcs/extended.vim
 
 try
-source ~/.vim_runtime/my_configs.vim
+source ~/.vim-runtime/my-configs.vim
 catch
 endtry' > ~/.vimrc
 
